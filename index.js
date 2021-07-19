a2605ad951a0407972dc958814294945c7910cc7
darshanaMadushan <darshanamsamarasinghe@gmail.com>
2021-07-19 21:30:48 +0530

    server on node

---
+++ index.js
@@ -0,0 +1,29 @@
+// dependencies
+const { futimesSync } = require("fs");
+const http = require("http");
+const url = require("url");
+
+
+// Server should respond all the requets with a string
+const server = http.createServer(function(request, respond) {
+
+    // Get the URL and parse it 
+    const passedURL = url.parse(request.url, true);
+
+    // Get the path
+    const path = passedURL.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Send the response
+    respond.end("Hellow World!\n")
+
+    // Log the requested path
+    console.log("Request recieved on path: " + trimmedPath);
+
+});
+
+
+// Server listen on port 3000
+server.listen(3000, function() {
+    console.log("Server is listening on the port: 3000")
+});
